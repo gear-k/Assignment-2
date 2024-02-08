a529e388ff2f4822ea50310ee5ddf08921319f13
Sundolby <sundolbyplayz@gmail.com>
2024-02-08 17:52:32 +0800

    changed color of leaderboard.

--- js/leaderbaord.js
+++ js/leaderbaord.js
@@ -1,49 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetchLeaderboardData();
-  });
-  
-  function fetchLeaderboardData() {
-    fetch('https://signup-828c.restdb.io/rest/signup', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-apikey': '65be5892c1ff3a2d670fe5a0'
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
+document.addEventListener("DOMContentLoaded", function () {
+  fetchLeaderboardData();
+});
+
+function fetchLeaderboardData() {
+  fetch("https://signup-828c.restdb.io/rest/signup", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-apikey": "65be5892c1ff3a2d670fe5a0",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
       populateLeaderboard(data);
     })
-    .catch(error => {
-      console.error('Error fetching data:', error);
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
-  }
-  
-  function populateLeaderboard(data) {
-    const sortedData = data.filter(item => item.leaderboard !== undefined)
-                           .sort((a, b) => b.leaderboard - a.leaderboard);
-    const tbody = document.getElementById('leaderboardBody');
-    tbody.innerHTML = ''; // Clear existing data
-  
-    sortedData.forEach((item, index) => {
-      if (index < 3) {
-        // Populate the placeholders for the top three
-        const placeHolder = document.getElementById(['firstPlace', 'secondPlace', 'thirdPlace'][index]);
-        placeHolder.innerHTML = `
+}
+
+function populateLeaderboard(data) {
+  const sortedData = data
+    .filter((item) => item.leaderboard !== undefined)
+    .sort((a, b) => b.leaderboard - a.leaderboard);
+  const tbody = document.getElementById("leaderboardBody");
+  tbody.innerHTML = ""; // Clear existing data
+
+  sortedData.forEach((item, index) => {
+    if (index < 3) {
+      // Populate the placeholders for the top three
+      const placeHolder = document.getElementById(
+        ["firstPlace", "secondPlace", "thirdPlace"][index]
+      );
+      placeHolder.innerHTML = `
           <th scope="row">${index + 1}</th>
           <td>${item.username}</td>
           <td>Singapore</td>
           <td>${item.leaderboard}</td>
         `;
-      } else {
-        // Populate the rest of the table
-        const row = `<tr>
+    } else {
+      // Populate the rest of the table
+      const row = `<tr>
           <th scope="row">${index + 1}</th>
           <td>${item.username}</td>
           <td>Singapore</td>
           <td>${item.leaderboard}</td>
         </tr>`;
-        tbody.innerHTML += row;
-      }
-    });
-  }
+      tbody.innerHTML += row;
+    }
+  });
+}
